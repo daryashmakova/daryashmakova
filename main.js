a27f61aa2da19012909c69b1d0096f7ad3ace017
daryashmakova <90901599+daryashmakova@users.noreply.github.com>
2021-11-18 22:53:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import {GameView} from "./gameview.js"
+import {GameView, clickToTail, canvas} from "./gameview.js"
+import {move} from "./game.js"
 
 const initialState = [
     [4, 1, 3],
@@ -6,5 +7,14 @@ const initialState = [
     [8, 0, 6]]
 
 let s1 = initialState
-
 new GameView(s1)
+
+canvas.addEventListener('click', moveView)
+
+
+function moveView(event)
+{
+    const [i, j] = clickToTail(event.offsetX, event.offsetY)
+    s1 = move(s1, i, j)
+    new GameView(s1)
+}
