f5d3d66d9095e604e3ad9818732275ff10161131
daryashmakova <90901599+daryashmakova@users.noreply.github.com>
2021-11-18 22:54:04 +0700

    Update gameview.js

--- gameview.js
+++ gameview.js
@@ -1,18 +1,18 @@
-const canvas = document.querySelector('#canvas')
+export const canvas = document. querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 const d = 3
 const tileWidth = canvas.width / d
-const tileHeight = canvas.height
+const tileHeight = canvas.height / d
 const tileColor = '#ccccff'
 const canvasColor = '#eee'
 canvas.style.backgroundColor = canvasColor
 
-export class Tile
+class Tile
 {
     constructor(i, j, n)
     {
         this.row = i
-        this.row = j
+        this.col = j
         this.val = n
     }
     
@@ -32,6 +32,31 @@ export class Tile
             ctx.fillText(this.val, this.col * tileWidth + tileWidth / 2, this.row * tileHeight + tileHeight / 2)
         }
     }
+}
+
+
+export class GameView
+{
+    constructor(state)
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        this.board = [new Array(d), new Array(d), new Array(d)]
+        for(let i = 0; i < d; i++)
+        {
+            for(let j = 0; j < d; j++)
+            {
+                this.board[i][j] = new Tile(i, j, state[i][j])
+                this.board[i][j].draw()
+            }
+        }
+    }
+}
+
+export function clickToTail(x, y)
+{
+    const j = Math.floor(x / tileWidth)
+    const i = Math.floor(y / tileHeight)
+    return [i,j]
 }
 
 
